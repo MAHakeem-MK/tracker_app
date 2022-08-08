f313380dffeddf4d764dc9d8172d273979d2f0ef
MAHakeem-MK <dev.mahakeemmk@gmail.com>
2022-08-08 11:32:58 +0530

    forground location fetching.

--- lib/main.dart
+++ lib/main.dart
@@ -2,47 +2,89 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
+import 'package:geolocator/geolocator.dart';
 
 import 'firebase_options.dart';
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  // await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Position? _position;
+
+  Future<Position> _determinePosition() async {
+    LocationPermission permission;
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    return await Geolocator.getCurrentPosition();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _determinePosition().then((value) {
+      setState(() {
+        _position = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     const providerConfigs = [EmailProviderConfiguration()];
 
     return MaterialApp(
       initialRoute:
-          FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/profile',
+          FirebaseAuth.instance.currentUser == null ? '/sign-in' : '/home',
       routes: {
         '/sign-in': (context) {
           return SignInScreen(
             providerConfigs: providerConfigs,
             actions: [
               AuthStateChangeAction<SignedIn>((context, state) {
-                Navigator.pushReplacementNamed(context, '/profile');
+                Navigator.pushReplacementNamed(context, '/home');
               }),
             ],
           );
         },
-        '/profile': (context) {
-          return ProfileScreen(
-            providerConfigs: providerConfigs,
-            actions: [
-              SignedOutAction((context) {
-                Navigator.pushReplacementNamed(context, '/sign-in');
-              }),
-            ],
+        '/home': (context) {
+          return Scaffold(
+            appBar: AppBar(title: const Text("Tracker")),
+            body: Center(
+              child: Text("${_position?.latitude} ${_position?.longitude}"),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: const Icon(Icons.my_location),
+              onPressed: () {
+                _determinePosition().then((value) {
+                  setState(() {
+                    _position = value;
+                  });
+                });
+              },
+            ),
           );
         },
       },
